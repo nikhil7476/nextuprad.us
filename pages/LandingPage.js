7b905fd9c5dde17e75fb85b666b94276cd4fc3e6
piyush022 <piyushverma022@gmail.com>
2024-05-13 21:51:56 +0530

    chnged video name

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -20,7 +20,7 @@ const LandingPage = () => {
       <section className={`${styles.section2piy} `}>
         <video
           className="w-100 bannerVideo"
-          src="/banVid.webm"
+          src="/BanVid.webm"
           autoPlay
           muted
           loop
