7d6fbc373a36a7234165491617bf22a5b09618cc
piyush022 <piyushverma022@gmail.com>
2024-05-13 16:29:45 +0530

    added tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+import { GoogleTagManager } from "@next/third-parties/google";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <GoogleTagManager gtmId="G-NZ5VGKXPYG" />
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ pages/podcast.js
@@ -0,0 +1,5 @@
+const Podcast = () => {
+  return <></>;
+};
+
+export default Podcast;
