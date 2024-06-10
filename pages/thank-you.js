735180e6992fc2dc1fb7d3566eada0064bb63299
Arjit <srivastava.arjit66@gmail.com>
2024-06-10 20:37:00 +0530

    thankyoupage

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -6,11 +6,11 @@ const ThankYou = () => {
             <div className="wrapper-1">
                 <div className="wrapper-2">
                     <h1>Thank you !</h1>
-                    <p>Thanks for subscribing to our news letter.  </p>
-                    <p>you should receive a confirmation email soon  </p>
+                    <p>Thanks for subscribing to our news letter. </p>
+                    <p>You should receive a confirmation email soon </p>
                     <button className="go-home" onClick={()=>router.push('/', { scroll: false })
 }>
-                        go home
+                        Go Home
                     </button>
                 </div>
                 <div className="footer-like">
