e391def30dd33c8f56f3f02a3588599b46305ba8
SudhanshuHRI <salil221254@gmail.com>
2024-06-10 19:43:34 +0530

    thankyou page added

---
+++ pages/thank-you.js
@@ -0,0 +1,26 @@
+import { useRouter } from 'next/navigation'
+const ThankYou = () => {
+    const router = useRouter()
+    return (
+        <div className='content'>
+            <div className="wrapper-1">
+                <div className="wrapper-2">
+                    <h1>Thank you !</h1>
+                    <p>Thanks for subscribing to our news letter.  </p>
+                    <p>you should receive a confirmation email soon  </p>
+                    <button className="go-home" onClick={()=>router.push('/', { scroll: false })
+}>
+                        go home
+                    </button>
+                </div>
+                <div className="footer-like">
+                    <p>Email not received?
+                        <a href="#">Click here to send again</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ThankYou;
