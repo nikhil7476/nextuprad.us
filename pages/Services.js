322053fd2feb099459cc89615f48a7b89fe96a49
pradeepuiux <developer6.nextupgrad@gmail.com>
2024-06-25 17:18:42 +0530

    css

---
+++ pages/Services.js
@@ -0,0 +1,25 @@
+const Services = () => {
+    return (  <>
+    <div className="container my-5">
+
+    <h1>We help <span>great minds</span> to explore their way</h1>
+    <div className="services-bigimages">
+  <img
+    className="tab-image-full-width"
+    src="/services-big.webp"
+    style={{
+      willChange: "transform",
+      transform:
+        "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+      transformStyle: "preserve-3d"
+    }}
+  />
+</div>
+
+
+    </div>
+    
+    </> );
+}
+ 
+export default Services;
