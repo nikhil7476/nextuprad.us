36ce3526f669c5f191b8e45cbcacc039ef751708
pradeepuiux <developer6.nextupgrad@gmail.com>
2024-06-27 15:23:57 +0530

    css

--- pages/Services.js
+++ pages/Services.js
@@ -1,29 +1,33 @@
 const Services = () => {
-    return (  <>
+    return (<>
 
-<main id="sect">
-          <div className="sect1">
-          <h1 className="serviceheadeing">We help <span>great minds</span> to explore their way</h1>
-            <div className="overlay" />
-          </div>
+        <main id="sect">
+            <div className="sect1">
+                <h1 className="serviceheadeing">We help <span>great minds</span> to explore their way</h1>
+                <div className="overlay" />
+            </div>
         </main>
 
-    <div className="container my-5">
-        <div className="row">
+        <div className="container my-5">
+            <div className="row">
 
-        <div className="sec-1-1-2 col-6 services-projects"> <h3>Your projects deserve the best!</h3>
-<p>Dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia.</p>
-<ul className="projectliste">
-<li>Praesentium voluptatum classical</li>
-<li>Hampden-sydney college in virginia,</li>
-<li>Finibus bonorum et malorum</li>
-</ul>
-</div>
-        <div className="col-6 serviceprojectsimg"><img src="/services-right.jpg" alt="" /></div>
+                <div className="sec-1-1-2 col-6 services-projects"
+                    data-aos="zoom-in"
+                    data-aos-delay="150"
+                    data-aos-duration="1500"> 
+                    <h3>Your projects deserve the best!</h3>
+                    <p>Dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia.</p>
+                    <ul className="projectliste">
+                        <li>Praesentium voluptatum classical</li>
+                        <li>Hampden-sydney college in virginia,</li>
+                        <li>Finibus bonorum et malorum</li>
+                    </ul>
+                </div>
+                <div className="col-6 serviceprojectsimg"><img src="/services-right.jpg" alt="" /></div>
 
-        </div>
-        
-    {/* <div className="services-bigimages">
+            </div>
+
+            {/* <div className="services-bigimages">
   <img
     className="tab-image-full-width"
     src="/services-big.webp"
@@ -35,31 +39,33 @@ const Services = () => {
     }}
   />
 </div> */}
-</div>
-<div className="datascroll">    
-    <marquee
-  behavior="scroll"
-  direction="left"
-  onmouseover="this.stop();"
-  onmouseout="this.start();"
->
-  Web development&nbsp;&nbsp;&nbsp;&nbsp;Brand design&nbsp;&nbsp;&nbsp;&nbsp;Busniess growth&nbsp;&nbsp;&nbsp;&nbsp;Mobile app design&nbsp;&nbsp;&nbsp;&nbsp;Website development&nbsp;&nbsp;&nbsp;&nbsp;Video production&nbsp;&nbsp;&nbsp;&nbsp;Digital marketing
-</marquee>
-</div>
-<h2 className="servicesubheadeing">You won’t be disappointed with<span>Service</span></h2>
-<ul className="serviceslisted">
-    <li><a href="/hire-dedicated-developer/">  <img src="/services.jpg" alt="" /><span>Hire Dedicated Developer</span></a></li>
-    <li><a href="/website-design-development-services/">  <img src="/services1.jpg" alt="" /><span>Website Development</span></a></li>
-    <li><a href="/software-development-services">  <img src="/services2.jpg" alt="" /><span>Software Development</span></a></li>
-    <li><a href="/mobile-app-development-services">  <img src="/services3.jpg" alt="" /><span>Mobile App Development</span></a></li>
-    <li><a href="/digital-marketing-services">  <img src="/services4.jpg" alt="" /><span>Digital Marketing</span></a></li>
+        </div>
+        <div class="scroll-container">
+        <div class="scroll-content">
+            <div class="scroll-item">Web development</div>
+            <div class="scroll-item">Brand design</div>
+            <div class="scroll-item">Business growth</div>
+            <div class="scroll-item">Mobile app design</div>
+            <div class="scroll-item">Website development</div>
+            <div class="scroll-item">Video production</div>
+            <div class="scroll-item">Digital marketing</div>
+            
+        </div>
+    </div>
+        <h2 className="servicesubheadeing">You won’t be disappointed with<span>Service</span></h2>
+        <ul className="serviceslisted">
+            <li><a href="/hire-dedicated-developer/">  <img src="/services.jpg" alt="" /><span>Hire Dedicated Developer</span></a></li>
+            <li><a href="/website-design-development-services/">  <img src="/services1.jpg" alt="" /><span>Website Development</span></a></li>
+            <li><a href="/software-development-services">  <img src="/services2.jpg" alt="" /><span>Software Development</span></a></li>
+            <li><a href="/mobile-app-development-services">  <img src="/services3.jpg" alt="" /><span>Mobile App Development</span></a></li>
+            <li><a href="/digital-marketing-services">  <img src="/services4.jpg" alt="" /><span>Digital Marketing</span></a></li>
 
-</ul>
+        </ul>
 
 
-    
-    
-    </> );
+
+
+    </>);
 }
- 
+
 export default Services;
